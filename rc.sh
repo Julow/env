ebcb1e2291f733a7759d887ddb821d1e2312a4fb
juloo <Julow@users.noreply.github.com>
2014-12-04 15:53:55 +0100

    Fix go when path contains spaces

--- rc.sh
+++ rc.sh
@@ -52,7 +52,7 @@ function save()
 function go()
 {
 	if [ -f ~/save_path/$1.save_path ]; then
-		cd `cat ~/save_path/$1.save_path`;
+		cd "`cat ~/save_path/$1.save_path`";
 	else
 		echo "The save $1 does not exists.";
 	fi
