bb4dfde1c8e99b54e8f2b58300dbab729b892901
jaguillo <juloo.dsi@gmail.com>
2015-09-09 22:23:35 +0200

    Generate init.sh during installation

--- install.sh
+++ install.sh
@@ -1,32 +1,57 @@
 #!/bin/bash
 
 ENV_PATH="`pwd`"
+
 INIT_FILE="$ENV_PATH/init.sh"
+INIT_PATH="$ENV_PATH/init"
+
 TOOLS_PATH="$ENV_PATH/tools"
 
-function install()
+# Create the init file
+function create_init()
+{
+	echo "# Init env
+
+# tools
+export PATH=\"$PATH:$TOOLS_PATH\"
+
+# aliases
+source \"$INIT_PATH/base.sh\"
+source \"$INIT_PATH/git_aliases.sh\"
+source \"$INIT_PATH/save-go.sh\"
+source \"$INIT_PATH/extra_aliases.sh\"
+source \"$INIT_PATH/42_aliases.sh\"
+
+# prompt
+source \"$INIT_PATH/prompt.sh\"
+" > $INIT_FILE
+}
+
+# Copy a file
+function install_file()
 {
 	echo "Install $1 to $2"
 	cp "$ENV_PATH/$1" "$2"
 }
 
-function install_init()
+# Put in line in a file
+# if the file does not already contains it
+function install_line()
 {
-	if grep "juloo env" "$1" > /dev/null; then
-		echo "Init already installed in $1"
+	if grep "$1" "$2" > /dev/null 2> /dev/null; then
+		echo "$2 already up to date"
 	else
-		echo "Install init in $1"
-		echo "
-# juloo env
-export PATH=\"$PATH:$TOOLS_PATH\"
-if [[ -f \"$INIT_FILE\" ]]; then
-	source \"$INIT_FILE\"
-fi" >> $1
+		echo "Update $2"
+		echo >> $2
+		echo "$1" >> $2
 	fi
 }
 
-install "conf_gitconfig" "$HOME/.gitconfig"
-install "conf_gitignore" "$HOME/.gitconfig_excludes"
+# Main
 
-install_init "$HOME/.bashrc"
-install_init "$HOME/.zshrc"
+install_file "conf_gitconfig" "$HOME/.gitconfig"
+install_file "conf_gitignore" "$HOME/.gitconfig_excludes"
+
+create_init
+install_line "source \"$INIT_FILE\"" "$HOME/.bashrc"
+install_line "source \"$INIT_FILE\"" "$HOME/.zshrc"
