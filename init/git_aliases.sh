c463d68ae12360b173b2855925608107fa010590
jaguillo <juloo.dsi@gmail.com>
2016-01-20 20:40:45 +0100

    Improve t (git status) alias

--- init/git_aliases.sh
+++ init/git_aliases.sh
@@ -24,7 +24,6 @@
 # r <file1> [file2...]
 # (Cancel (git add) for a file) + (Print the status)
 #
-alias t="git status -sb"
 alias c="git commit -m"
 alias amend="git commit --amend --no-edit"
 
@@ -52,10 +51,75 @@ function pl()
 
 function a()
 {
-	git add --all $@ && git status -bs
+	git add --all $@ && t
 };
 
 function r()
 {
-	git reset -- HEAD -q $@ && git status -bs
+	git reset -- HEAD -q $@ && t
+};
+
+function t()
+{
+	echo '
+import re
+
+stats = {}
+
+max_name_len = 25
+total_add = 0
+total_del = 0
+
+for line in """'"$(git diff --numstat HEAD)"'""".split("\n"):
+	m = re.match("^\s*(\d+)\s+(\d+)\s+(.+)$", line)
+	if m != None:
+		add = int(m.group(1))
+		rem = int(m.group(2))
+		name = m.group(3)
+		if len(name) > max_name_len:
+			max_name_len = len(name)
+		total_add += add
+		total_del += rem
+		stats[name] = (add, rem)
+
+total_file = 0
+total_untrack = 0
+
+for line in """'"$(git status -sb)"'""".split("\n"):
+	if line.startswith("##"):
+		print "\033[97m##\033[0m %s" % line[3:]
+	else:
+		m = re.match("^(.)(.)\s+(.+)$", line)
+		if m == None:
+			print line
+		else:
+			if m.group(1) == "?":
+				status = "\033[31m??\033[0m"
+			else:
+				status = "\033[32m%s\033[31m%s\033[0m" % (m.group(1), m.group(2))
+			name = m.group(3)
+			if name in stats:
+				add, rem = stats[name]
+				print "%s %-*s | \033[32m%2d+ \033[31m%2d-\033[0m" % (
+					status,
+					max_name_len, name,
+					add, rem
+				)
+				total_file += 1
+			else:
+				print "%s \033[90m%s\033[0m" % (status, name)
+				total_untrack += 1
+
+total_str = "%d files" % total_file
+if total_untrack > 0:
+	total_str += " \033[90m+ %d untracked\033[0m" % total_untrack
+	max_name_len += len("\033[90m\033[0m")
+
+if total_file > 0:
+	print "\033[97m##\033[0m %-*s | \033[32m%2d+ \033[31m%2d-\033[0m" % (max_name_len, total_str, total_add, total_del)
+else:
+	print "\033[97m##\033[0m %s" % total_str
+
+' | python
+
 };
