218b6cbf04bf43aeb60b615561fe004c58e736e5
jaguillo <juloo.dsi@gmail.com>
2016-05-16 11:09:37 +0200

    .

--- init/base.sh
+++ init/base.sh
@@ -58,10 +58,10 @@ export SUBL="subl"
 if [[ "`uname`" == "Darwin" ]]; then
 	if [[ -f "/Applications/Sublime Text 3.app/Contents/SharedSupport/bin/subl" ]]; then
 		export SUBL="/Applications/Sublime\ Text\ 3.app/Contents/SharedSupport/bin/subl"
+	elif [[ -f "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ]]; then
+		export SUBL="/Applications/Sublime\ Text\.app/Contents/SharedSupport/bin/subl"
 	elif [[ -f "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ]]; then
 		export SUBL="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
-	elif [[ -f "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ]]; then
-		export SUBL="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 	fi
 fi
 
