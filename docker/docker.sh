660b2251b16d863a810ba318f75ce97af08f9ef1
jaguillo <juloo.dsi@gmail.com>
2016-05-20 19:28:09 +0200

    Improve docker.sh + Fix Dockerfile

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-set -e
-
 IMAGE_NAME="juloo_image"
 CONTAINER_NAME="juloo"
+SHARE_FOLTER="/Users/Shared/juloo:/juloo"
+RUN_CMD="bash"
 
-docker build -t "$IMAGE_NAME" .
-docker stop "$CONTAINER_NAME" 2> /dev/null && docker rm "$CONTAINER_NAME" || true
-docker run -P --privileged -d --name "$CONTAINER_NAME" "$IMAGE_NAME"
+if [[ "$1" == "re" ]]; then
+	docker kill "$CONTAINER_NAME" 2> /dev/null
+	docker rm "$CONTAINER_NAME" 2> /dev/null
+	CONTAINER_STATE=""
+else
+	CONTAINER_STATE=$(docker inspect -f {{.State.Running}} "$CONTAINER_NAME" 2> /dev/null)
+fi
 
-printf "IP: " ; docker-machine ip default
-printf "Port: " ; docker port "$CONTAINER_NAME" 22 | sed 's/.*://'
+if [[ "$CONTAINER_STATE" == "true" ]] ; then
+	echo "Already running"
+	docker exec -i -t "$CONTAINER_NAME" "$RUN_CMD"
+elif [[ "$CONTAINER_STATE" == "false" ]]; then
+	echo "Restart"
+	docker start -i "$CONTAINER_NAME"
+else
+	docker build -t "$IMAGE_NAME" .
+	docker run -P --privileged -it -v "$SHARE_FOLTER" --name "$CONTAINER_NAME" "$IMAGE_NAME" "$RUN_CMD"
+fi
