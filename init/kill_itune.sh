fb75058bf44eb1f97b129a50549280be8d7e18af
jaguillo <jaguillo@student.42.fr>
2016-11-30 15:21:56 +0100

    kill_itune by default

--- init/kill_itune.sh
+++ init/kill_itune.sh
@@ -1,6 +1,9 @@
 # kill itune
 
-cat << "KILL_ITUNE_END"
-launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
-launchctl unload -w /System/Library/LaunchDaemons/com.apple.apsd.plist
+if type launchctl >/dev/null
+then
+	cat << "KILL_ITUNE_END"
+launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2>/dev/null
+launchctl unload -w /System/Library/LaunchDaemons/com.apple.apsd.plist 2>/dev/null
 KILL_ITUNE_END
+fi
