c8919007baefd70c24720c3d4fccb416935e426d
juloo <juloo.dsi@gmail.com>
2018-04-17 18:42:36 +0200

    Override tmux copy/paste shortcuts to use the system clipboard instead

--- session/tmux.sh
+++ session/tmux.sh
@@ -1,9 +1,11 @@
 # .tmux.conf
 
 cat > "$HOME/.tmux.conf" <<"EOF"
-
 set -g prefix C-Space
 unbind C-b
+
 setw -g mode-keys vi
 
+bind -T copy-mode-vi enter send-keys -X copy-pipe-and-cancel "xclip -selection clipboard"
+bind "]" run "xclip -o -sel clipboard | tmux load-buffer -; tmux paste-buffer"
 EOF
