32147bfc9e9caa9fb2869eb596322d2e8a68f679
juloo <juloo.dsi@gmail.com>
2018-07-07 09:57:30 +0200

    tmux: Enable mouse mode

--- session/tmux.sh
+++ session/tmux.sh
@@ -1,11 +1,19 @@
 # .tmux.conf
 
 cat > "$HOME/.tmux.conf" <<"EOF"
+# Change prefix to C-Space
 set -g prefix C-Space
 unbind C-b
 
+# Vi bindings
 setw -g mode-keys vi
 
+# Copy and paste use x clipboard
 bind -T copy-mode-vi enter send-keys -X copy-pipe-and-cancel "xclip -selection clipboard"
 bind "]" run "xclip -o -sel clipboard | tmux load-buffer -; tmux paste-buffer"
+
+# Mouse mode
+set -g mouse on
+# Ending mouse selection does not close selection mode
+unbind -n -Tcopy-mode-vi MouseDragEnd1Pane
 EOF
