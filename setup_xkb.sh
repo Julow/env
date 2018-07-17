981806aae4ad90fb9f5ce8e3cabe18276311918c
juloo <juloo.dsi@gmail.com>
2018-07-17 22:44:55 +0200

    Remove xkb configs

--- setup_xkb.sh
+++
@@ -1,9 +0,0 @@
-xkbcomp -I$HOME/_env/xkb - $DISPLAY << KEYMAP_END
-xkb_keymap {
-	xkb_keycodes	{ include "evdev+aliases(qwerty)" };
-	xkb_types		{ include "complete" };
-	xkb_compat		{ include "complete" };
-	xkb_symbols		{ include "pc+us+inet(evdev)+juloo(custom)" };
-	xkb_geometry	{ include "pc(pc105)" };
-};
-KEYMAP_END
