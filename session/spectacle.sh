a8aeff7ac3301f2b5c999e1901adc0a7c87cc296
Jules Aguillon <jaguillon@advanced-schema.com>
2018-11-30 13:27:34 +0100

    Change spectacle bindings

--- session/spectacle.sh
+++ session/spectacle.sh
@@ -12,23 +12,23 @@ cp "$SESSION_DIR/spectacle.plist" "$SPECTACLE_PREFERENCES"
 
 cat << EOF > "$SPECTACLE_SHORTCUTS"
 [
-	{ "shortcut_key_binding": "alt+cmd+keypaddecimal", "shortcut_name": "MoveToPreviousThird" },
-	{ "shortcut_key_binding": "alt+cmd+keypadminus", "shortcut_name": "MakeSmaller" },
-	{ "shortcut_key_binding": null, "shortcut_name": "RedoLastMove" },
-	{ "shortcut_key_binding": "alt+cmd+keypad9", "shortcut_name": "MoveToUpperRight" },
-	{ "shortcut_key_binding": "alt+cmd+keypad2", "shortcut_name": "MoveToBottomHalf" },
-	{ "shortcut_key_binding": "alt+cmd+keypadmultiply", "shortcut_name": "MoveToNextDisplay" },
-	{ "shortcut_key_binding": "alt+cmd+keypad8", "shortcut_name": "MoveToTopHalf" },
-	{ "shortcut_key_binding": "alt+cmd+keypad1", "shortcut_name": "MoveToLowerLeft" },
-	{ "shortcut_key_binding": "alt+cmd+keypadplus", "shortcut_name": "MakeLarger" },
-	{ "shortcut_key_binding": "alt+cmd+keypaddivide", "shortcut_name": "MoveToPreviousDisplay" },
-	{ "shortcut_key_binding": null, "shortcut_name": "UndoLastMove" },
-	{ "shortcut_key_binding": "alt+cmd+keypad5", "shortcut_name": "MoveToFullscreen" },
-	{ "shortcut_key_binding": "alt+cmd+keypad0", "shortcut_name": "MoveToNextThird" },
-	{ "shortcut_key_binding": "alt+cmd+keypad4", "shortcut_name": "MoveToLeftHalf" },
-	{ "shortcut_key_binding": "alt+cmd+keypadequals", "shortcut_name": "MoveToCenter" },
-	{ "shortcut_key_binding": "alt+cmd+keypad6", "shortcut_name": "MoveToRightHalf" },
-	{ "shortcut_key_binding": "alt+cmd+keypad3", "shortcut_name": "MoveToLowerRight" },
-	{ "shortcut_key_binding": "alt+cmd+keypad7", "shortcut_name": "MoveToUpperLeft" }
+  { "shortcut_name" : "RedoLastMove", "shortcut_key_binding" : null },
+  { "shortcut_name" : "MakeSmaller", "shortcut_key_binding" : "alt+shift+-" },
+  { "shortcut_name" : "MoveToPreviousThird", "shortcut_key_binding" : null },
+  { "shortcut_name" : "MoveToUpperRight", "shortcut_key_binding" : null },
+  { "shortcut_name" : "MoveToBottomHalf", "shortcut_key_binding" : null },
+  { "shortcut_name" : "MoveToNextDisplay", "shortcut_key_binding" : "alt+shift+0" },
+  { "shortcut_name" : "MoveToTopHalf", "shortcut_key_binding" : null },
+  { "shortcut_name" : "MoveToLowerLeft", "shortcut_key_binding" : null },
+  { "shortcut_name" : "MakeLarger", "shortcut_key_binding" : "alt+shift+=" },
+  { "shortcut_name" : "UndoLastMove", "shortcut_key_binding" : "alt+shift+p" },
+  { "shortcut_name" : "MoveToPreviousDisplay", "shortcut_key_binding" : "alt+9" },
+  { "shortcut_name" : "MoveToFullscreen", "shortcut_key_binding" : "alt+shift+return" },
+  { "shortcut_name" : "MoveToNextThird", "shortcut_key_binding" : null },
+  { "shortcut_name" : "MoveToLeftHalf", "shortcut_key_binding" : "alt+shift+[" },
+  { "shortcut_name" : "MoveToCenter", "shortcut_key_binding" : "alt+shift+delete" },
+  { "shortcut_name" : "MoveToRightHalf", "shortcut_key_binding" : "alt+shift+]" },
+  { "shortcut_name" : "MoveToLowerRight", "shortcut_key_binding" : null },
+  { "shortcut_name" : "MoveToUpperLeft", "shortcut_key_binding" : null }
 ]
 EOF
