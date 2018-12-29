9e86668730e71857e1f73e0f08638bc12f44a7a7
juloo <juloo.dsi@gmail.com>
2018-12-29 14:12:07 +0000

    Vimrc

---
+++ session/vimrc.sh
@@ -0,0 +1,32 @@
+# Vim config
+
+cat > "$HOME/.vimrc" <<"EOF"
+filetype plugin on
+filetype indent on
+
+set encoding=utf8
+set ignorecase
+set hlsearch
+set showmatch
+
+set wildmenu
+
+syntax enable
+
+set backspace=eol,start,indent
+
+" Indent
+set smarttab
+set shiftwidth=4
+set tabstop=4
+set ai
+set si
+
+set wrap
+
+" Fold
+set foldenable
+set foldmethod=indent
+set foldlevelstart=99
+nnoremap <space> za
+EOF
