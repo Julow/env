dca955e215c7620e97337aa91c14f3b0cdd5765e
juloo <juloo.dsi@gmail.com>
2019-02-05 12:34:04 +0100

    Move vimrc to its own file

--- session/vim.sh
+++ session/vim.sh
@@ -15,13 +15,10 @@ VIM_DIR="$HOME/.vim"
 
 mkdir -p "$VIM_DIR/"{,autoload,bundle}
 
-RET=101
-
 # Pathogen
 
 PATHOGEN="$VIM_DIR/autoload/pathogen.vim"
 if ! [[ -f "$PATHOGEN" ]]; then
-	RET=0
 	echo "Instal pathogen"
 	curl -sLo "$PATHOGEN" https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 fi
@@ -31,10 +28,11 @@ fi
 for repo in $PACKAGES; do
 	DST="$VIM_DIR/bundle/${repo##*/}"
 	if ! [[ -d "$DST" ]]; then
-		RET=0
 		echo "Install ${repo##*/}"
 		git clone -q "$repo" "$DST"
 	fi
 done
 
-exit $RET
+# Vimrc
+
+cp "$SESSION_DIR/vimrc" "$HOME/.vimrc"
