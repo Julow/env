7a7f906706c9835d12c3da5c45a9058822cf9430
Jules Aguillon <juloo.dsi@gmail.com>
2019-02-05 18:57:29 +0100

    Add brightness.sh to tools
    Change backlight brightness

---
+++ tools/brightness.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Increments or decrements the brightness
+# `brightness.sh 10` increase brightness by 10%
+# `brightness.sh -10` decrease brightness by 10%
+
+set -e
+
+INC="$1"
+
+set /sys/class/backlight/*/brightness
+
+if [[ $# -eq 0 ]]; then
+	echo "Cannot change brightness" >&2
+	exit 1
+fi
+
+CUR=`cat "$1"`
+MAX=`cat "${1%brightness}max_brightness"`
+NEW=$((MAX * INC / 100 + CUR))
+
+if [[ $NEW -gt $MAX ]]; then NEW=$MAX; fi
+if [[ $NEW -lt 0 ]]; then NEW=0; fi
+
+echo $NEW > "$1"
+echo "$NEW / $MAX"
