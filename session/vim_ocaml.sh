37e7f15fa42cb80b6ea50e0dbe4686dc9aabb37a
Jules Aguillon <juloo.dsi@gmail.com>
2019-03-11 22:17:03 +0100

    Vim: OCaml: Use ocp-index instead of merlin

--- session/vim_ocaml.sh
+++ session/vim_ocaml.sh
@@ -1,6 +1,6 @@
 # OCaml merlin plugin for Vim
 
-MERLIN_PATH="$OPAM_SWITCH_PREFIX/share/merlin/vim"
+OCPINDEX_PATH="$OPAM_SWITCH_PREFIX/share/ocp-index/vim"
 
 if ! which vim &>/dev/null; then
 	exit 100
@@ -8,19 +8,20 @@ fi
 
 VIM_DIR="$HOME/.vim"
 
-PLUGIN_PATH="$VIM_DIR/bundle/ocaml_merlin"
+PLUGIN_PATH="$VIM_DIR/bundle/ocaml_ocp_index"
 
-if [[ -d $MERLIN_PATH ]] && ! [[ -e $PLUGIN_PATH ]]; then
-	ln -sf "$MERLIN_PATH" "$PLUGIN_PATH"
+if [[ -d $OCPINDEX_PATH ]] && ! [[ -e $PLUGIN_PATH ]]; then
+	ln -sf "$OCPINDEX_PATH" "$PLUGIN_PATH"
 fi
 
 mkdir -p "$VIM_DIR/ftplugin/"
 
 cat > "$VIM_DIR/ftplugin/ocaml.vim" <<"EOF"
-" Clear merlin highlighting
-autocmd User ClearHighlight :silent! :MerlinClearEnclosing
-
 let no_plugin_maps = 1
 
 setlocal commentstring=(*\ %s\ *)
+
+call ocpindex#init()
+
+nmap K :call ocpindex#print()<return>
 EOF
