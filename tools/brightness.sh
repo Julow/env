1dcb0b29859c537364fd9ffc9e0c1eb8bcfa3b1b
Jules Aguillon <juloo.dsi@gmail.com>
2019-03-13 13:56:58 +0100

    Progress bar notification

--- tools/brightness.sh
+++ tools/brightness.sh
@@ -27,5 +27,5 @@ echo "$NEW / $MAX"
 
 if which notify-send &>/dev/null; then
 	VAL=$((NEW * 100 / MAX))
-	notify-send -a "Brightness" -u low -h int:value:"$VAL" " "
+	notify-progress.sh "Brightness" "$VAL"
 fi
