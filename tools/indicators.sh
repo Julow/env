c51b4ed4640a2d6d9d4fe5888510ac3354274779
juloo <juloo.dsi@gmail.com>
2019-03-20 14:18:55 +0100

    Indicators: Improve battery indicator

--- tools/indicators.sh
+++ tools/indicators.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+# Date
 DATE=`date +"<b>%H:%M</b> %A %d %B (week %V)"`
 dunstify -r 101010 -a "Indicator" -u low "$DATE"
 
-eval `acpi -b | sed -e 's/^Battery [0-9]*: \(.*\), \([0-9]\+\)%.*$/STATE="\1" LEVEL="\2"/'`
-dunstify -r 101011 -a "Indicator" -u low "<b>Battery</b> `progress-bar.sh "$LEVEL"` $STATE"
+# Battery
+acpi -b | while read line; do
+	if [[ $line =~ ^Battery\ [0-9]*:\ (.*), ([0-9]+)% ]]; then
+		PROGRESS=`progress-bar.sh "${BASH_REMATCH[2]}"`
+		MSG="<b>Battery</b> $PROGRESS ${BASH_REMATCH[1]}"
+		dunstify -r 101011 -a "Indicator" -u low "$MSG"
+	fi
+done
 
+# Wifi
 WIFI=`netctl list | grep -e "^\S"`
 if [[ -n $WIFI ]]; then
 	dunstify -r 101012 -a "Indicator" -u low "<b>Wifi</b> $WIFI"
