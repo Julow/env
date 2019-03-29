946e5605eafd45dc190ddff97b09a7330512dc71
Jules Aguillon <juloo.dsi@gmail.com>
2019-03-29 11:27:49 +0100

    Fix bash prompt
    SHLVL code was wrong, enclose control sequences

--- session/initrc/4-prompt.sh
+++ session/initrc/4-prompt.sh
@@ -1,24 +1,24 @@
 C_ESC="`printf '\033'`"
 
-C_RESET="${C_ESC}[39m${SUFFIX}"
-C_RED="${C_ESC}[31m${SUFFIX}"
-C_CYAN="${C_ESC}[36m${SUFFIX}"
-C_GRAY="${C_ESC}[90m${SUFFIX}"
-C_LGREEN="${C_ESC}[92m${SUFFIX}"
+C_RESET="\[${C_ESC}[39m\]"
+C_RED="\[${C_ESC}[31m\]"
+C_CYAN="\[${C_ESC}[36m\]"
+C_GRAY="\[${C_ESC}[90m\]"
+C_LGREEN="\[${C_ESC}[92m\]"
 
 function _ps1_status()
 {
-	STATUS="$?"
-	if [[ $STATUS -ne 0 ]]; then
-		echo -n "${C_RED}[${STATUS}] "
+	status="$?"
+	if [[ $status -ne 0 ]]; then
+		echo -n "$C_RED[$status] "
 	fi
 };
 
-SHLVL=""
-if [[ "$SHLVL" -gt "1" ]]; then SHLVL="${C_GRAY}${SHLVL}> "; fi
+SHLVL_=""
+if [[ $SHLVL -ge 1 ]]; then SHLVL_="$C_GRAY$SHLVL> "; fi
 
 CLEAR_TITLE=`printf '\033]0;\007'`
 BELL=`if [[ -n $TMUX ]]; then printf '\a'; fi`
 
 # Bash
-export PROMPT_COMMAND='export PS1="$BELL$CLEAR_TITLE$SHLVL`_ps1_status`${C_CYAN}\h ${C_LGREEN}\w${C_RESET} "'
+export PROMPT_COMMAND='export PS1="\[$BELL$CLEAR_TITLE\]$SHLVL_`_ps1_status`$C_CYAN\h $C_LGREEN\w$C_RESET "'
