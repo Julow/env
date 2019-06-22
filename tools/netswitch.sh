3ff44b6653d1ad9a9cbfa9880e296938a9c862bd
Jules Aguillon <juloo.dsi@gmail.com>
2019-06-22 21:22:04 +0200

    tools/netswitch.sh: More interactive

--- tools/netswitch.sh
+++ tools/netswitch.sh
@@ -4,26 +4,37 @@
 
 set -e
 
-PROFILE_LIST=`netctl list`
-
+PROFILE_LIST=
 # Empty if none
-CURRENT_PROFILE=""
-while IFS="" read line; do
-	if ! [[ $line = "  "* ]]; then
-		CURRENT_PROFILE=${line:2}
-		break
-	fi
-done <<<"$PROFILE_LIST"
+CURRENT_PROFILE=
+
+load_profiles ()
+{
+	PROFILE_LIST=`netctl list`
+	CURRENT_PROFILE=""
+	while IFS="" read line; do
+		if ! [[ $line = "  "* ]]; then
+			CURRENT_PROFILE=${line:2}
+			break
+		fi
+	done <<<"$PROFILE_LIST"
+}
+
+wait_failed ()
+{
+	echo "Failed, press any key to continue"
+	read -n 1
+}
 
 # Remove menu
 
 remove_menu ()
 {
 	REMOVE=`echo "$PROFILE_LIST" | select_in`
+	if [[ -z $REMOVE ]]; then return 0; fi
 	REMOVE=${REMOVE:2}
-	[[ -n $REMOVE ]]
 	echo "Removing $REMOVE"
-	su -c "netctl stop '$REMOVE'; netctl disable '$REMOVE'; rm '/etc/netctl/$REMOVE'"
+	su -c "netctl stop '$REMOVE'; netctl disable '$REMOVE'; rm '/etc/netctl/$REMOVE'" || wait_failed
 }
 
 # Main menu
@@ -39,31 +50,37 @@ list ()
 	echo "> [Remove]"
 }
 
-SWITCH_TO=`list | select_in`
-SWITCH_TO=${SWITCH_TO:2}
+while true; do
 
-case "$SWITCH_TO" in
-	"")
-		exit 1
-		;;
-	"[Disconnect all]")
-		echo "Disconnecting"
-		su -c "netctl stop-all"
-		;;
-	"[Disconnect $CURRENT_PROFILE]")
-		echo "Disconnecting from $CURRENT_PROFILE"
-		su -c "netctl stop '$CURRENT_PROFILE'"
-		;;
-	"[New]")
-		su -c "wifi-menu"
-		;;
-	"[Remove]")
-		remove_menu
-		;;
-	*)
-		echo "Connecting to $SWITCH_TO"
-		su -c "netctl switch-to '$SWITCH_TO'"
-		echo "Wait online"
-		netctl wait-online "$SWITCH_TO"
-		;;
-esac
+	clear
+
+	load_profiles
+
+	SWITCH_TO=`list | select_in`
+	if [[ -z $SWITCH_TO ]]; then break; fi
+	SWITCH_TO=${SWITCH_TO:2}
+
+	case "$SWITCH_TO" in
+		"[Disconnect all]")
+			echo "Disconnecting"
+			su -c "netctl stop-all" || wait_failed
+			;;
+		"[Disconnect $CURRENT_PROFILE]")
+			echo "Disconnecting from $CURRENT_PROFILE"
+			su -c "netctl stop '$CURRENT_PROFILE'" || wait_failed
+			;;
+		"[New]")
+			su -c "wifi-menu" || wait_failed
+			;;
+		"[Remove]")
+			remove_menu
+			;;
+		*)
+			echo "Connecting to $SWITCH_TO"
+			su -c "netctl switch-to '$SWITCH_TO'" || wait_failed
+			echo "Wait online"
+			netctl wait-online "$SWITCH_TO" || wait_failed
+			;;
+	esac
+
+done
