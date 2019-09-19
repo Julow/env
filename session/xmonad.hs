fd5fa6071c8f39d19aadf68786e72c5727362f34
juloo <juloo.dsi@gmail.com>
2019-09-20 00:55:32 +0200

    XMonad: Use spaces instead of tabs

--- session/xmonad.hs
+++ session/xmonad.hs
@@ -29,9 +29,9 @@ import qualified Data.Map as M
 data Prompt_autocomplete = Prompt_autocomplete String
 
 instance XPrompt Prompt_autocomplete where
-	showXPrompt (Prompt_autocomplete prompt) = prompt
-	commandToComplete _ c = c
-	nextCompletion _ = getNextCompletion
+  showXPrompt (Prompt_autocomplete prompt) = prompt
+  commandToComplete _ c = c
+  nextCompletion _ = getNextCompletion
 
 home_dir = io $ getEnv "HOME"
 
@@ -51,11 +51,11 @@ screen_timeout_locked = 20
 set_dpms timeout = "xset dpms 0 0 " ++ show timeout
 
 lock_screen = spawn (
-		(set_dpms screen_timeout_locked) ++ ";"
-		++ "slock;"
-		++ (set_dpms screen_timeout) ++ ";"
-		++ "bash ~/.xreloadrc"
-	)
+    (set_dpms screen_timeout_locked) ++ ";"
+    ++ "slock;"
+    ++ (set_dpms screen_timeout) ++ ";"
+    ++ "bash ~/.xreloadrc"
+  )
 
 -- ========================================================================== --
 -- Browser
@@ -72,31 +72,30 @@ take_screenshot_interactive = safeSpawn "screenshot.sh" ["interactive"]
 -- Password prompt
 -- Ask the password manager
 
+compl_no_empty _ "" = return []
+compl_no_empty f s = f s
+
 password_prompt prompt_conf = do
-	home <- home_dir
-	let pass_dir = home ++ "/notes/pass/"
-	let pass_script = home ++ "/notes/_pass.sh"
-	ps <- io $ getDirectoryContents pass_dir
-	let ps' = filter (not . isPrefixOf ".") ps
-	let compl = mkComplFunFromList' ps'
-	-- Show nothing if input is empty
-	let compl' s = case s of
-		"" -> return []
-		_ -> compl s
-	let get_password p = safeSpawn pass_script [ "get", pass_dir ++ p ]
-	mkXPrompt (Prompt_autocomplete "Password: ") prompt_conf compl' get_password
+  home <- home_dir
+  let pass_dir = home ++ "/notes/pass/"
+  let pass_script = home ++ "/notes/_pass.sh"
+  ps <- io $ getDirectoryContents pass_dir
+  let ps' = filter (not . isPrefixOf ".") ps
+  let compl = compl_no_empty $ mkComplFunFromList' ps'
+  let get_password p = safeSpawn pass_script [ "get", pass_dir ++ p ]
+  mkXPrompt (Prompt_autocomplete "Password: ") prompt_conf compl get_password
 
 -- ========================================================================== --
 -- Preset prompt
 -- Prompt to execute a shell script located in the ~/.presets directory
 
 preset_prompt prompt_conf = do
-	home <- home_dir
-	let preset_dir = home ++ "/.presets/"
-	ws <- io $ getDirectoryContents preset_dir
-	let ws' = filter (flip notElem [ ".", ".." ]) ws
-	let open w = spawn ("source \"" ++ preset_dir ++ w ++ "\"")
-	mkXPrompt (Prompt_autocomplete "Preset: ") prompt_conf (mkComplFunFromList' ws') open
+  home <- home_dir
+  let preset_dir = home ++ "/.presets/"
+  ws <- io $ getDirectoryContents preset_dir
+  let ws' = filter (flip notElem [ ".", ".." ]) ws
+  let open w = spawn ("source \"" ++ preset_dir ++ w ++ "\"")
+  mkXPrompt (Prompt_autocomplete "Preset: ") prompt_conf (mkComplFunFromList' ws') open
 
 -- ========================================================================== --
 -- Window prompt
@@ -104,15 +103,15 @@ preset_prompt prompt_conf = do
 -- Similar to Xmonad.Prompt.Window
 
 window_title ws w = do
-	name <- show <$> getName w
-	let tag = W.tag ws
-	return ("[" ++ tag ++ "] " ++ name)
+  name <- show <$> getName w
+  let tag = W.tag ws
+  return ("[" ++ tag ++ "] " ++ name)
 
 window_prompt prompt_conf = do
-	wm <- windowMap' window_title
-	let compl = mkComplFunFromList' (M.keys wm)
-	let action = flip whenJust (windows . W.focusWindow) . flip M.lookup wm
-	mkXPrompt (Prompt_autocomplete "Windows: ") prompt_conf compl action
+  wm <- windowMap' window_title
+  let compl = mkComplFunFromList' (M.keys wm)
+  let action = flip whenJust (windows . W.focusWindow) . flip M.lookup wm
+  mkXPrompt (Prompt_autocomplete "Windows: ") prompt_conf compl action
 
 -- ========================================================================== --
 -- Centered layout
@@ -121,40 +120,39 @@ window_prompt prompt_conf = do
 data CenteredLayout a = CenteredLayout Integer deriving (Show, Read)
 
 instance LayoutModifier CenteredLayout a where
-	handleMess (CenteredLayout step) m
-		| Just Shrink <- fromMessage m = modify_lr (-step)
-		| Just Expand <- fromMessage m = modify_lr step
-		| otherwise = return Nothing
-		where
-			modify_lr d = do
-				let m (Border t b r l) = Border t b (r + d) (l + d)
-				_ <- sendMessage (ModifyWindowBorder m)
-				return Nothing
+  handleMess (CenteredLayout step) m
+    | Just Shrink <- fromMessage m = modify_lr (-step)
+    | Just Expand <- fromMessage m = modify_lr step
+    | otherwise = return Nothing
+    where
+      modify_lr d = do
+        let m (Border t b r l) = Border t b (r + d) (l + d)
+        _ <- sendMessage (ModifyWindowBorder m)
+        return Nothing
 
 centered_full sp step =
-	ModifiedLayout (CenteredLayout step) $
-	spacingRaw False (Border 0 0 0 0) False (Border 0 0 sp sp) True $
-	Full
+  ModifiedLayout (CenteredLayout step) $
+  spacingRaw False (Border 0 0 0 0) False (Border 0 0 sp sp) True $
+  Full
 
 -- ========================================================================== --
 -- main
 
-prompt_conf = def
-	{
-		font = "xft:",
-		promptBorderWidth = 0,
-		height = 22,
-		position = CenteredAt 0.5 0.5,
-		promptKeymap = foldl (\m (k, a) -> M.insert k a m) emacsLikeXPKeymap [
-			((controlMask, xK_w), killWord Prev),
-			((controlMask, xK_Left), moveWord Prev),
-			((controlMask, xK_Right), moveWord Next)
-		]
-	}
+prompt_conf = def {
+  font = "xft:",
+  promptBorderWidth = 0,
+  height = 22,
+  position = CenteredAt 0.5 0.5,
+  promptKeymap = foldl (\m (k, a) -> M.insert k a m) emacsLikeXPKeymap [
+    ((controlMask, xK_w), killWord Prev),
+    ((controlMask, xK_Left), moveWord Prev),
+    ((controlMask, xK_Right), moveWord Next)
+  ]
+}
 
 on_start = do
-	init_wallpaper
-	safeSpawn "firefox" []
+  init_wallpaper
+  safeSpawn "firefox" []
 
 tabbed_theme = def {
   fontName = "xft:"
@@ -167,26 +165,26 @@ layout = minimize $ boringWindows $ tabbed (tiled_layout ||| centered_layout)
     tabbed = addTabs shrinkText tabbed_theme . subLayout [] Full
 
 main =
-	xmonad $ def
-	{
-		focusFollowsMouse = False,
-		borderWidth = 0,
-		startupHook = on_start,
-		logHook = updatePointer (0.99, 0.001) (0, 0),
-		layoutHook = layout,
-		manageHook = manageSpawn <+> manageHook def,
-		terminal = "xterm tmux"
-	} `additionalKeysP`
-	[
+  xmonad $ def
+  {
+    focusFollowsMouse = False,
+    borderWidth = 0,
+    startupHook = on_start,
+    logHook = updatePointer (0.99, 0.001) (0, 0),
+    layoutHook = layout,
+    manageHook = manageSpawn <+> manageHook def,
+    terminal = "xterm tmux"
+  } `additionalKeysP`
+  [
 
-		("M-S-l",					sendMessage MirrorShrink),
-		("M-S-h",					sendMessage MirrorExpand),
+    ("M-S-l", sendMessage MirrorShrink),
+    ("M-S-h", sendMessage MirrorExpand),
 
-		-- BoringWindows
-		("M-S-<Tab>",				focusUp),
-		("M-<Tab>",					focusDown),
-		("M-m",						markBoring),
-		("M-S-m",					clearBoring),
+    -- BoringWindows
+    ("M-S-<Tab>", focusUp),
+    ("M-<Tab>", focusDown),
+    ("M-m", markBoring),
+    ("M-S-m", clearBoring),
 
     -- SubTabbed
     ("M-u", withFocused (\w -> focusDown >> sendMessage (mergeDir id w))),
@@ -196,35 +194,35 @@ main =
 
     ("M-<Space>", toSubl NextLayout),
 
-		("M-d",						withFocused minimizeWindow),
-		("M-S-d",					withLastMinimized maximizeWindowAndFocus),
+    ("M-d", withFocused minimizeWindow),
+    ("M-S-d", withLastMinimized maximizeWindowAndFocus),
 
-		("<XF86AudioLowerVolume>",	safeSpawn "volume.sh" ["-5%"]),
-		("<XF86AudioRaiseVolume>",	safeSpawn "volume.sh" ["+5%"]),
-		("<XF86AudioMute>",			safeSpawn "volume.sh" ["toggle"]),
-		("<XF86AudioPlay>",			spawnOn "9" "playerctl play-pause || spotify.sh"),
-		("<XF86AudioPrev>",			safeSpawn "playerctl" ["previous"]),
-		("<XF86AudioNext>",			safeSpawn "playerctl" ["next"]),
+    ("<XF86AudioLowerVolume>", safeSpawn "volume.sh" ["-5%"]),
+    ("<XF86AudioRaiseVolume>", safeSpawn "volume.sh" ["+5%"]),
+    ("<XF86AudioMute>", safeSpawn "volume.sh" ["toggle"]),
+    ("<XF86AudioPlay>", spawnOn "9" "playerctl play-pause || spotify.sh"),
+    ("<XF86AudioPrev>", safeSpawn "playerctl" ["previous"]),
+    ("<XF86AudioNext>", safeSpawn "playerctl" ["next"]),
 
-		("<XF86MonBrightnessUp>",	safeSpawn "brightness.sh" ["5"]),
-		("<XF86MonBrightnessDown>",	safeSpawn "brightness.sh" ["-5"]),
+    ("<XF86MonBrightnessUp>", safeSpawn "brightness.sh" ["5"]),
+    ("<XF86MonBrightnessDown>", safeSpawn "brightness.sh" ["-5"]),
 
-		("M-z",						lock_screen),
+    ("M-z", lock_screen),
 
-		("M-S-<Backspace>",			safeSpawnProg web_browser),
+    ("M-S-<Backspace>", safeSpawnProg web_browser),
 
-		("M-p",						shellPrompt prompt_conf),
-		("M-S-p",					window_prompt prompt_conf),
-		("M-S-o",					preset_prompt prompt_conf),
+    ("M-p", shellPrompt prompt_conf),
+    ("M-S-p", window_prompt prompt_conf),
+    ("M-S-o", preset_prompt prompt_conf),
 
-		("M-;",						password_prompt prompt_conf),
+    ("M-;", password_prompt prompt_conf),
 
-		("M-`",						safeSpawn "indicators.sh" []),
+    ("M-`", safeSpawn "indicators.sh" []),
 
-		("M-S-s",					take_screenshot),
-		("M-s",						take_screenshot_interactive)
+    ("M-S-s", take_screenshot),
+    ("M-s", take_screenshot_interactive)
 
-	] `removeKeysP`
-	[
-		"M-S-q"
-	]
+  ] `removeKeysP`
+  [
+    "M-S-q"
+  ]
