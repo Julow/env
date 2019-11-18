94ac2fbee67b8d7924b9e4d3dbe9746900a15cff
juloo <juloo.dsi@gmail.com>
2019-11-19 00:30:59 +0100

    apod_wallpaper.sh: Use the API and generate details file
    Use the JSON API from api.nasa.gov instead of scrapping the website.
    Write details (title, explanation, etc..) to ~/.cache/apod.md

--- tools/apod_wallpaper.sh
+++ tools/apod_wallpaper.sh
@@ -5,23 +5,51 @@ set -e
 set_wallpaper () { feh --bg-fill "$1"; }
 
 IMG_DST="$HOME/.cache/apod.jpg"
+DETAILS_DST="$HOME/.cache/apod.md"
 
 # Temporarily use previous image
 if [[ -e $IMG_DST ]]; then
   set_wallpaper "$IMG_DST" || true
 fi
 
+# Log details and evetually errors
+exec &>"$DETAILS_DST"
+
 # Fetch lastest image
-BASE_URL="https://apod.nasa.gov/apod"
-PAGE_DST=/tmp/apod.html
+API_URL="https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-11-17"
+JSON_DST="/tmp/apod.json"
 
-curl -L --retry 10 -o "$PAGE_DST" "$BASE_URL/astropix.html"
+curl --silent -L --retry 10 -o "$JSON_DST" "$API_URL"
 
-IMG_PATH=`sed --quiet -E '/^.*<a href="(image\/.+)".*$/{s//\1/;p}' </tmp/apod.html`
+eval `jq -r '@sh "
+URL=\(.url)
+HDURL=\(.hdurl)
+MEDIA_TYPE=\(.media_type)
+TITLE=\(.title)
+EXPLANATION=\(.explanation)
+DATE=\(.date)
+"' "$JSON_DST"`
 
-curl -o "$IMG_DST" "$BASE_URL/$IMG_PATH"
+URL=${HDURL:=$URL}
 
-set_wallpaper "$IMG_DST"
+echo "Astronomy Picture of the Day ($DATE)"
+echo
+if [[ $MEDIA_TYPE = image ]]; then
+  echo "# $TITLE"
+else
+  echo "# $TITLE ($MEDIA_TYPE)"
+fi
+echo
+echo "$URL"
+echo
+echo "$EXPLANATION"
 
-#
-rm "$PAGE_DST"
+case "$MEDIA_TYPE" in
+  image)
+    curl --silent -o "$IMG_DST" "$URL"
+    set_wallpaper "$IMG_DST"
+    ;;
+
+  *)
+    echo; echo "Cannot handle media type '$MEDIA_TYPE'" ;;
+esac
