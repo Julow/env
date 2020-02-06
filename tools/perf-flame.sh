44cd9c46f2d4c4daad8be82b22ba38516024eae2
Jules Aguillon <juloo.dsi@gmail.com>
2020-02-06 18:42:10 +0100

    perf-flame.sh: Don't override

--- tools/perf-flame.sh
+++ tools/perf-flame.sh
@@ -1,1 +1,18 @@
-perf script | stackcollapse-perf.pl | flamegraph.pl > perf.svg
+#!/usr/bin/env bash
+
+ensure_available ()
+{
+  if [[ -e "$1" ]]; then
+    local d="$1.old"
+    ensure_available "$d"
+    echo "$1 already exists, renaming it to $d"
+    mv "$1" "$d"
+  fi
+}
+
+d=perf.svg
+ensure_available "$d"
+
+perf script | stackcollapse-perf.pl | flamegraph.pl > "$d"
+
+echo "Generated $d"
