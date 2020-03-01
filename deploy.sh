e2d2750597f9463cc5f4bbb054ad902f3dcbfbb4
juloo <juloo.dsi@gmail.com>
2020-03-01 19:27:39 +0100

    Deploy to VM

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,17 @@ config=$(realpath "$(dirname "$0")/configuration.nix")
 
 [[ -e $config ]]
 
-su -c "NIXOS_CONFIG='$config' nixos-rebuild switch"
+cd /tmp
+
+case "$1" in
+  vm)
+    NIXOS_CONFIG=$config nixos-rebuild build-vm
+    result/bin/run-nixos-vm
+    ;;
+
+  "")
+    su -c "NIXOS_CONFIG='$config' nixos-rebuild switch"
+    ;;
+
+  *) echo "Usage: $0 [vm]" >&2 ;;
+esac
