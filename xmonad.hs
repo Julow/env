a601b9d6620cb448e1e2d9d082ef594826f4037e
juloo <juloo.dsi@gmail.com>
2020-03-01 20:24:08 +0100

    Xmonad: Use light-locker

--- xmonad.hs
+++ xmonad.hs
@@ -49,16 +49,7 @@ compl_no_empty f s = f s
 -- ========================================================================== --
 -- Lock screen
 
-screen_timeout = 0
-screen_timeout_locked = 10
-
-set_dpms timeout = "xset dpms 0 0 " ++ show timeout
-
-lock_screen = spawn (
-    (set_dpms screen_timeout_locked) ++ ";"
-    ++ "slock;"
-    ++ (set_dpms screen_timeout)
-  )
+lock_screen = spawn "light-locker-command -l"
 
 -- ========================================================================== --
 -- Browser
