7435129c8fd50f825cab1aef5c739ab1ab441ae1
juloo <juloo.dsi@gmail.com>
2020-03-27 22:53:26 +0100

    Home: Add Steam, enable 32bit opengl and pulseaudio support

--- configuration-home.nix
+++ configuration-home.nix
@@ -3,7 +3,7 @@
   host_name = "jules-pc";
   main_user = "juloo";
 
-  extra_config = {
+  extra_config = { pkgs, ... }: {
     # Use the systemd-boot EFI boot loader.
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
@@ -14,6 +14,14 @@
 
     # Video drivers
     services.xserver.videoDrivers = [ "nvidia" ];
+
+    # Extra packages
+    environment.systemPackages = with pkgs; [ steam ];
+
+    # Support for 32bit games
+    hardware.opengl.driSupport32Bit = true;
+    hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+    hardware.pulseaudio.support32Bit = true;
 
   };
 }
