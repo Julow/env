06f0b51a2a06d1c2e593497ebc794c9a9020e030
Jules Aguillon <juloo.dsi@gmail.com>
2020-08-21 18:30:35 +0200

    Simplify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,45 +2,18 @@
 
 set -e
 
-case $HOSTNAME in
-  "jules-work") config=./configuration-work.nix ;;
-  "jules-pc") config=./configuration-home.nix ;;
+# Useful default arguments
+if [[ $# -eq 0 ]]; then
+  case $HOSTNAME in
+    "jules-work") config=./configuration-work.nix ;;
+    "jules-pc") config=./configuration-home.nix ;;
 
-  *)
-    echo "Unknown hostname $HOSTNAME" >&2
-    exit 1
-esac
+    *)
+      echo "Unknown hostname $HOSTNAME" >&2
+      exit 1
+  esac
 
-build ()
-{
-  local target="$1"
-  shift
-  echo "Building $config" >&2
-  # Clear NIX_PATH and other variables
-  env -i \
-  nix-build --no-out-link -A "$target" "$@" '<nixpkgs/nixos>' \
-    -I nixos-config="$config" -I nixpkgs=nixpkgs
-}
+  set deploy local "$config"
+fi
 
-case "$1" in
-  vm)
-    result=$(build vm)
-    "$result"/bin/run-*-vm
-    ;;
-
-  trace)
-    result=$(build system --show-trace)
-    echo "Built $result"
-    ;;
-
-  "")
-    result=$(build system)
-    echo "Built $result"
-    echo "Switching..."
-    su -c \
-"nix-env --profile /nix/var/nix/profiles/system --set '$result' && \
-/nix/var/nix/profiles/system/bin/switch-to-configuration switch"
-    ;;
-
-  *) echo "Usage: $0 [vm|trace|]" >&2 ;;
-esac
+nixos-deploy "$@"
