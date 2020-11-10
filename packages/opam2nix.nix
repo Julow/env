e2cb5476dafb10feb43afcf063b29eca0124f669
Jules Aguillon <juloo.dsi@gmail.com>
2020-11-10 16:08:22 +0100

    Update nixpkgs and overlay

--- packages/opam2nix.nix
+++ packages/opam2nix.nix
@@ -3,11 +3,9 @@
 let
   src = fetchgit {
     url = "https://github.com/timbertson/opam2nix";
-    rev = "764bcb4a4b6591b74f98c44f6b0cf76cb0db6fd8";
-    sha256 = "0fxx8zb2n9jnn2xwbx9sblnhkd8myiahp6g909wynirl6yx3cymy";
+    rev = "f6b45ed9e95e468f2971f7fee9dd5abbbc1d7577";
+    sha256 = "0294jyqjnvvy9z79vxhkb2b3pwzg4wn7mg1bsb2j9m3rv2p6dxbs";
   };
 in
 
-pkgs.callPackage src {
-  ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_08;
-}
+pkgs.callPackage src {}
