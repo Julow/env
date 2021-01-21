4eaf3105d027f4fe1ad9fb9111111a85542c2303
Jules Aguillon <juloo.dsi@gmail.com>
2021-01-21 12:48:02 +0100

    tools/indicators.sh: Network status with nmcli

--- tools/indicators.sh
+++ tools/indicators.sh
@@ -1,20 +1,36 @@
 #!/usr/bin/env bash
 
+N=0
+
+indicator ()
+{
+  dunstify -r "10101$N" -a "Indicator" -u low "" "$1"
+  N=$((N+1))
+}
+
 # Date
 DATE=`date +"<b>%H:%M</b> %A %d %B (week %V)"`
-dunstify -r 101010 -a "Indicator" -u low "" "$DATE"
+indicator "$DATE"
 
 # Battery
-acpi -b | while read line; do
+while read line; do
 	if [[ $line =~ ^Battery\ [0-9]*:\ (.*),\ ([0-9]+)%.*$ ]] &&
 		[[ ${BASH_REMATCH[2]} -ne 0 ]]; then
 		PROGRESS=`progress-bar.sh "${BASH_REMATCH[2]}"`
-		MSG="<b>Battery</b> $PROGRESS ${BASH_REMATCH[1]}"
-		dunstify -r 101011 -a "Indicator" -u low "" "$MSG"
+    indicator "<b>Battery</b> $PROGRESS ${BASH_REMATCH[1]}"
 	fi
-done
+done < <(acpi -b)
 
-# Wifi
-if which iwgetid &>/dev/null && SSID=`iwgetid -r` 2>/dev/null; then
-	dunstify -r 101012 -a "Indicator" -u low "" "<b>Wifi</b> $SSID"
+# Network
+if which nmcli &>/dev/null; then
+  CONN=0
+  while IFS=: read _ type state _ _ _ ssid _; do
+    if [[ $state = connected ]]; then
+      indicator "<b>Connected to $type</b> $ssid"
+      CONN=1
+    fi
+  done < <(nmcli -g all d)
+  if [[ $CONN -eq 0 ]]; then
+    indicator "<b>Not connected</b>"
+  fi
 fi
